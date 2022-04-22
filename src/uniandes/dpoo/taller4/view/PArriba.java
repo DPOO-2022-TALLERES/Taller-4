bb61fcbb9349ba10a30b0e67dddae612f812796f
PacolasPL <82538410+PacolasPL@users.noreply.github.com>
2022-04-22 06:36:30 -0500

    PArriba

--- src/uniandes/dpoo/taller4/view/PArriba.java
+++ src/uniandes/dpoo/taller4/view/PArriba.java
@@ -2,23 +2,30 @@ package uniandes.dpoo.taller4.view;
 
 import java.awt.Color;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
-public class PArriba extends JPanel implements ItemListener{
+public class PArriba extends JPanel implements ItemListener, ActionListener{
 	
 	private String[] listamatriculas = {"5 x 5", "6 x 6", "7 x 7", "8 x 8", "9 x 9", "10 x 10"};
 	private ButtonGroup grupoBotones;
+	private JRadioButton facil, medio, dificil ;
+	private int difSelection;
 	private String selection;
 	private JComboBox<String> tamaniocuadricula = new JComboBox<>(listamatriculas);
+	private JButton changeDif;
 	Color lightblue = new Color(51,136,255);
+	
 	
 	PArriba(){
 		this.setLayout(new GridBagLayout());
@@ -29,17 +36,21 @@ public class PArriba extends JPanel implements ItemListener{
 		JLabel dificultad = new JLabel("Dificultad");
 		dificultad.setForeground(Color.white);
 		
-		JRadioButton facil = new JRadioButton("Facil");
+		facil = new JRadioButton("Facil");
 		facil.setForeground(Color.white);
 		facil.setBackground(lightblue);
 		
-		JRadioButton medio = new JRadioButton("Medio");
+		medio = new JRadioButton("Medio");
 		medio.setForeground(Color.white);
 		medio.setBackground(lightblue);
 		
-		JRadioButton dificil = new JRadioButton("Dificil");
+		dificil = new JRadioButton("Dificil");
 		dificil.setForeground(Color.white);
 		dificil.setBackground(lightblue);
+		
+		changeDif = new JButton();
+		changeDif.setText("Cambiar");
+		
 		
 		this.grupoBotones = new ButtonGroup();
 		grupoBotones.add(facil);
@@ -52,9 +63,12 @@ public class PArriba extends JPanel implements ItemListener{
 		this.add(facil);
 		this.add(medio);
 		this.add(dificil);
-		tamaniocuadricula.addItemListener(this);
-		
+		this.add(changeDif);
+		grupoBotones.getSelection();
 		setBackground(lightblue);
+		
+		tamaniocuadricula.addItemListener(this);
+		changeDif.addActionListener(this);
 		
 		
 	}
@@ -64,8 +78,10 @@ public class PArriba extends JPanel implements ItemListener{
 		return tamanio;
 	}
 
-	public void getDificultad(){
-		
+	public int getDificultad(){
+		String d = Integer.toString(difSelection);
+		JOptionPane.showMessageDialog(null, d);
+		return difSelection;
 	}
 
 	@Override
@@ -74,6 +90,26 @@ public class PArriba extends JPanel implements ItemListener{
 		if (e.getSource() == tamaniocuadricula) {
 			selection = tamaniocuadricula.getSelectedItem().toString();
 
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == changeDif ) {
+			changeDificulty();
+		}
+		
+	
+	}
+	protected void changeDificulty() {
+		if (facil.isSelected()) {
+			difSelection = 5;
+		}
+		else if (medio.isSelected()) {
+			difSelection = 10;
+		}
+		else if (dificil.isSelected()) {
+			difSelection = 20;
 		}
 	}
 	
