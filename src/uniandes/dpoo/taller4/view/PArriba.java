fe34d917afbd055a5c30c4f2e59b42958f9bcfc2
Valeria <v.caro@uniandes.edu.co>
2022-04-21 23:15:32 -0500

    mas cambios

--- src/uniandes/dpoo/taller4/view/PArriba.java
+++ src/uniandes/dpoo/taller4/view/PArriba.java
@@ -1,5 +1,6 @@
 package uniandes.dpoo.taller4.view;
 
+import java.awt.Color;
 import java.awt.GridBagLayout;
 
 import javax.swing.ButtonGroup;
@@ -12,21 +13,32 @@ public class PArriba extends JPanel{
 	
 	private String[] listamatriculas = {"5x5", "6x6", "7x7", "8x8", "9x9", "10x10"};
 	private ButtonGroup grupoBotones;
+	Color lightblue = new Color(51,136,255);
+	
 	
 	PArriba(){
 		this.setLayout(new GridBagLayout());
 		
 		JLabel tamanio = new JLabel("Tamanio: ");
+		tamanio.setForeground(Color.white);
 		
 		JComboBox tamaniocuadricula = new JComboBox<>(listamatriculas);
 		
 		JLabel dificultad = new JLabel("Dificultad");
+		dificultad.setForeground(Color.white);
 		
 		JRadioButton facil = new JRadioButton("Fácil");
 		
+		facil.setBackground(lightblue);
+		facil.setForeground(Color.white);
+		
 		JRadioButton medio = new JRadioButton("Medio");
+		medio.setBackground(lightblue);
+		medio.setForeground(Color.white);
 		
 		JRadioButton dificil = new JRadioButton("Dificil");
+		dificil.setBackground(lightblue);
+		dificil.setForeground(Color.white);
 		
 		this.grupoBotones = new ButtonGroup();
 		grupoBotones.add(facil);
@@ -40,13 +52,13 @@ public class PArriba extends JPanel{
 		this.add(medio);
 		this.add(dificil);
 		
-
 		
-		
-		
+		setBackground(lightblue);
 		
 		
 		
 	}
+	
+	
 
 }
--- src/uniandes/dpoo/taller4/view/PBotones.java
+++ src/uniandes/dpoo/taller4/view/PBotones.java
@@ -1,5 +1,6 @@
 package uniandes.dpoo.taller4.view;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
@@ -14,23 +15,22 @@ import javax.swing.JPanel;
 
 public class PBotones extends JPanel{
 	GridBagConstraints constraints = new GridBagConstraints();
+	Color lightblue = new Color(51,136,255);
 
 	PBotones(){
 		this.setLayout(new GridBagLayout());
 		
-		int x,y;
-		
 		JButton buttonNuevo = new JButton("Nuevo");
-		addButton(buttonNuevo, x = 1, y = 0, buttonNuevo);
+		addButton(buttonNuevo, 1, 0, buttonNuevo);
 		
 		JButton buttonReiniciar = new JButton("Reiniciar");
-		addButton(buttonReiniciar , x = 1, y = 1, buttonReiniciar);
+		addButton(buttonReiniciar , 1,  1, buttonReiniciar);
 		
 		JButton buttonTop = new JButton("TOP - 10");
-		addButton(buttonTop, x = 1, y = 2,buttonTop);
+		addButton(buttonTop, 1, 2,buttonTop);
 		
 		JButton buttonCambiarJugador = new JButton("Cambiar Jugador");
-		addButton(buttonCambiarJugador, x = 1, y = 3, buttonCambiarJugador);
+		addButton(buttonCambiarJugador, 1, 3, buttonCambiarJugador);
 		
 	}
 	
@@ -40,6 +40,9 @@ public class PBotones extends JPanel{
         constraints.insets = new Insets(5, 0, 5, 0);
         constraints.fill = GridBagConstraints.HORIZONTAL;
         button.setAlignmentX(this.CENTER_ALIGNMENT);
+        button.setBackground(lightblue);
+        button.setForeground(Color.white);
+        
         add(component, constraints);
       }
 	
